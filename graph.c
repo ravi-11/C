55588f4b69f8bdb1fd7d33109605289ac614555a
Tino <33922973+TinoMadrid@users.noreply.github.com>
2019-03-14 11:24:18 -0700

    Update graph.c

--- graph.c
+++ graph.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -6,6 +7,7 @@
 bool adjMatrix[5][5];
 /*****************************************************************************/
 void read();
+uint8_t getNum(char a, uint8_t ret);
 void printMatrix();
 /*****************************************************************************/
 int main(int argc, char **argv)
@@ -20,10 +22,10 @@ int main(int argc, char **argv)
 void read()
 {
 	FILE *f = fopen("pairs.txt","r");
-	char buff[255];
-	int xFlag, yFlag;
-	long xLoc, yLoc;
-	char *ptr;
+	char symb;
+	uint8_t x = 255, y = 255, ret = 255;
+	uint8_t xLoc, yLoc;
+	bool firstIteration = false, secondIteration = false;
 
 	if(f == NULL)
 	{
@@ -32,68 +34,55 @@ void read()
 	}
 	else
 	{
-		while(!feof(f))
+		while((symb=getc(f)) != EOF)
 		{
-			/* grab the current line from text file and put into buff */
-			fgets(buff,6,f);
-			//printf("%s",buff); //displays what is in buffer
-			
-			/* now scan that line that is in buff */
-			for(int i = 0; i < 6; i++)
+			printf("x is: %d\n",x);
+			x = getNum(symb, ret);
+			if(x >= 0 && x <= 9)
 			{
-				/* if we have not found our first number go into here */
-				if(xFlag != 1)
+				if(firstIteration == false)
 				{
-					/* if the current character is a number */
-					if(buff[i] >= '0' && buff[i] <= '9')
-					{
-						printf("First number of string is: %c\t", buff[i]);
-						long num = strtol(buff,&ptr,10);
-
-						/* we have found our x Location of adjMatrix */
-						xLoc = num;
-						/* we have found our first number, now on to second */
-						xFlag = 1;
-					}
-
+					//printf("In firstIteration xLoc: %d\t", x);
+					xLoc = x;
+					firstIteration = true;
 				}
-				/* if we already have our first number */
+				else if(secondIteration == false)
+				{
+					//printf("In secondIteration yLoc: %d\t", x);
+					yLoc = x;
+					secondIteration = true;
+				}
 				else
 				{
-					/* is the current character a number? */
-					if(buff[i] >= 0 && buff[i] <= 9)
-					{
-						//printf("Second number of string is: %d\t", buff[i]);
-						/* second number is our y Location of adjMatrix */
-						long num = strtol(buff,&ptr,10);
-						yLoc = num;
-						/* we now have our second number */
-						yFlag = 1;
-					}
-
-				}
-				/* if we have both x and y location numbers */
-				if(xFlag == 1 && yFlag == 1)
-				{
-					//printf("x:%d, y:%d",buff[xLoc], buff[yLoc]);
-					//printf("\n");
-					/* set the relationship between both numbers to true */
+					printf("\n");
+					printf("adjMatrix[%d][%d]\n",xLoc, yLoc);
 					adjMatrix[xLoc][yLoc] = true;
-				}
+					firstIteration = false;
+					secondIteration = false;
+					printf("\n");
+				}		
 			}
-			printf("\n");
-
-			/* restart for the next string that is read in */
-			xFlag = 0;
-			yFlag = 0;
 		}
 	}
 	fclose(f);
 
 }
 /*****************************************************************************/
+uint8_t getNum(char a, uint8_t ret)
+{
+	unsigned char b;
+	b = (unsigned char)a;
+	if(b >= '0' && b <= '9')
+	{
+		ret = b - '0';
+		return ret;
+	}
+	return -1;
+}
+/*****************************************************************************/
 void printMatrix()
 {
+	printf("\n");
 	for(int i = 0; i < 5; i++)
 	{
 		for(int j = 0; j < 5; j++)
